c96e76ad65d729dfd5f6aede1e17dc162dedf0af
Arturo Escudero <arturo.escudero@ericsson.com>
2014-12-29 14:17:29 +0100

    Grouping all the test for the Command class in the same file

--- test_command_base_class.py
+++ test_command_base_class.py
@@ -1,7 +1,11 @@
 import unittest
 from command import Command
 
+class DummyCommand(Command):
+   pass
+
 class TestCommandBaseClass(unittest.TestCase):
+   """ Test basic things with the constructor """
    def test_create_command(self):
       """ Testing the constructor with one argument """
       _list = "list"
@@ -15,3 +19,49 @@ class TestCommandBaseClass(unittest.TestCase):
       cmd = Command(_list, _aliases)
       self.assertEqual(cmd.aliases, _aliases);
 
+class TestCommandMatch(unittest.TestCase):
+   def test_argv1_matches_with_cmd(self):
+      """ Testing that the command in argv[1]('list) matches with Command('list') """
+      _pathname = "/foo/bar/devtool"
+      _list     = "list"
+      myArgv    = [_pathname, _list, "--help"] 
+      cmd = DummyCommand(_list)
+      self.assertTrue( cmd.match(myArgv) );
+
+   def test_argv1_does_not_match_with_cmd(self):
+      """ Testing that the command in argv[1]('show') does not match with Command('list') """
+      _pathname = "/foo/bar/devtool"
+      _list     = "list"
+      myArgv    = [_pathname, "show"] 
+      cmd = DummyCommand(_list)
+      self.assertFalse( cmd.match(myArgv) );
+
+   def test_argv1_does_not_match_with_cmd_but_matches_with_aliases_cmd(self):
+      """ Testing that the alias command in argv[1]('ls') matches with some the aliases cmds of Command('list', ['ls', 'lst']) """
+      _pathname = "/foo/bar/devtool"
+      _list     = "list"
+      _ls       = "ls"
+      _aliases  = [_ls, "lst"]
+      myArgv    = [_pathname, _ls, "--help"] 
+      cmd = DummyCommand(_list, _aliases)
+      self.assertTrue( cmd.match(myArgv) );
+
+   def test_argv1_does_not_match_with_cmd_neither_aliases(self):
+      """ Testing that the alias command in argv[1]('show') does not match with neither the command or the aliases commands of Command('list', ['ls', 'lst']) """
+      _pathname = "/foo/bar/devtool"
+      _list     = "list"
+      _ls       = "ls"
+      _aliases  = [_ls, "lst"]
+      myArgv    = [_pathname, _ls, "--help"] 
+      cmd = DummyCommand(_list, _aliases)
+      self.assertTrue( cmd.match(myArgv) );
+
+# Test Backlog
+"""
+Testing that apply raises an AttributeException when calling it as a method of the Command class
+Testing that apply raises an AttributeException when addArguments is not implemented by the calling subclass
+Testing that apply raises an AttributeException when applyCommand is not implemented by the calling subclass
+Testing that the default basic options for the provided parser are verbose and quiet
+Testing that the implementing command is able to add more options
+Testing that applyCommand
+"""

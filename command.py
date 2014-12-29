3ad131e3ff8b8afe08ad865a0976846ee7ae240f
Arturo Escudero <arturo.escudero@ericsson.com>
2014-12-29 01:08:13 +0100

    Add the first idea for Command::match
    A little bit of documentation driven design here... I have just place the idea,
    and then I will place the code.

--- command.py
+++ command.py
@@ -1,3 +1,6 @@
+class MisspelledCommandException(Exception):
+   pass
+
 class Command:
    def __init__(self, command, aliases = []):
       """
@@ -5,3 +8,14 @@ class Command:
       """
       self.command = command
       self.aliases = aliases
+   
+   def match(self, args):
+      """ Tells whether the command encoded in the argv[0] matches with the
+          current value of self.command, or any of the aliases in the
+          self.aliases list.
+          If none of the previous gives a match, the method will find how close
+          the command in argv[0] is to self.command (not to the aliases), and
+          if it is 85% close to the command (some misspelling perhaps), match
+          will raise a MisspelledCommandException.
+      """
+      pass

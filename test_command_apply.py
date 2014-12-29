b9e7b2347f90c9e8c01da7f78c18afdfcc4f58b8
Arturo Escudero <arturo.escudero@ericsson.com>
2014-12-29 09:16:57 +0100

    Add extra check for the Command::apply method
    I have realised here that I'm overdoing this a bit. However this steps have
    helped me to discover that I don't want Command::apply to behave that way.

--- test_command_apply.py
+++ test_command_apply.py
@@ -6,7 +6,7 @@ class DummyCommand(Command):
 
 class TestCommandApply(unittest.TestCase):
    def test_apply_raises_NotImplementedError_when_used_directly(self):
-      """ Test that apply raises a NotImplementedError Exception when calling it from an instance of the Command class """
+      """ Testing that apply raises a NotImplementedError Exception when calling it from an instance of the Command class """
       _pathname = "/foo/bar/devtool"
       _list     = "list"
       myArgv    = [_pathname, _list, "--help"] 
@@ -15,7 +15,7 @@ class TestCommandApply(unittest.TestCase):
          cmd.apply(myArgv) 
 
    def test_apply_raises_NotImplementedError_when_subclass_does_not_implements_apply(self):
-      """ Test that apply raises a NotImplementedError Exception if the Command subclass does not implement the method """
+      """ Testing that apply raises a NotImplementedError Exception if the Command subclass does not implement the method """
       _pathname = "/foo/bar/devtool"
       _list     = "list"
       myArgv    = [_pathname, _list, "--help"] 
@@ -23,4 +23,13 @@ class TestCommandApply(unittest.TestCase):
       with self.assertRaises(NotImplementedError):
          cmd.apply(myArgv) 
 
-""" Test that the apply method from the Command subclass is called correctly"""
+   def test_apply_raises_NotImplementedError_when_subclass_does_not_implements_apply(self):
+      """ Testing that the apply method from the Command subclass is called correctly"""
+      _pathname = "/foo/bar/devtool"
+      _list     = "list"
+      myArgv    = [_pathname, _list, "--help"] 
+      class ListCommand(Command):
+         def apply(self, args):
+            pass
+      cmd = ListCommand(_list)
+      cmd.apply(myArgv) 

e7cbdcfe012e9d2e1db22c83084d539b8d15272a
Arturo Escudero <arturo.escudero@ericsson.com>
2014-12-29 09:02:55 +0100

    Add extra check for the apply method
    Add new test to check that the exception will be raised if the user forgets to
    implement it.
    
    Change name of the test methods for this Test class.

--- test_command_apply.py
+++ test_command_apply.py
@@ -5,7 +5,7 @@ class DummyCommand(Command):
    pass
 
 class TestCommandApply(unittest.TestCase):
-   def test_argv1_matches_with_cmd(self):
+   def test_apply_raises_NotImplementedError_when_used_directly(self):
       """ Test that apply raises a NotImplementedError Exception when calling it from an instance of the Command class """
       _pathname = "/foo/bar/devtool"
       _list     = "list"
@@ -14,5 +14,13 @@ class TestCommandApply(unittest.TestCase):
       with self.assertRaises(NotImplementedError):
          cmd.apply(myArgv) 
 
-""" Test that apply raises a NotImplementedError Exception if the Command subclass does not implemen the method """
+   def test_apply_raises_NotImplementedError_when_subclass_does_not_implements_apply(self):
+      """ Test that apply raises a NotImplementedError Exception if the Command subclass does not implement the method """
+      _pathname = "/foo/bar/devtool"
+      _list     = "list"
+      myArgv    = [_pathname, _list, "--help"] 
+      cmd = DummyCommand(_list)
+      with self.assertRaises(NotImplementedError):
+         cmd.apply(myArgv) 
+
 """ Test that the apply method from the Command subclass is called correctly"""

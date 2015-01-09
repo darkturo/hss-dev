6707ae1c21ccbef1414b1fb36742f1df754aef13
Arturo Escudero <arturo.escudero@ericsson.com>
2015-01-09 14:00:43 +0100

    Remove MisspelledCommandException and update documentation
    - Remove MisspelledCommandException, this exception will be raised by the main
      program instead.
    - Implemented __str__ method for the Command class.
    - Clean the description of Command.match. I now have discarded the idea of
      provide a fuzzy comparisson from the class itself.

--- lib/command.py
+++ lib/command.py
@@ -1,9 +1,6 @@
 # Arturo Escudero <arturo.escudero@ericsson.com> 2015
 import argparse 
 
-class MisspelledCommandException(Exception):
-   pass
-
 class Command:
    """
    Command Base class.
@@ -55,6 +52,9 @@ class Command:
       self.aliases = aliases
       self.options = ()
 
+   def __str__(self):
+      return ",".join( self.getCommandAssociatedStrings() )
+
    def getCommandAssociatedStrings(self):
       """
       Returns a list with the identifier for the command and the list of
@@ -67,11 +67,6 @@ class Command:
       Tells whether the command encoded in the argv[1]
       matches with the current value of self.command, or any
       of the aliases in the self.aliases list.
-      If none of the previous gives a match, the method will
-      find how close the command in argv[1] is to
-      self.command (not to the aliases), and if it is 85%
-      close to the command (some misspelling perhaps), match
-      will raise a MisspelledCommandException.
       """
       if (len(args) <= 1):
          return False
